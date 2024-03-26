2c610f1c761a4acd8a44e58523033444c0ef5ec7
Luiz Felipe Balaminute <lfbalaminute@hotmail.com>
2024-03-26 12:48:28 -0300

    ...

--- lista_dupla.c
+++ lista_dupla.c
@@ -7,6 +7,11 @@ typedef struct no {
     struct no * anterior;
 } t_no;
 
+typedef struct lista {
+    t_no * primeiro;
+    t_no * ultimo;
+} t_lista;
+
 t_no * constroi_no (int info) {
     t_no * no = (t_no *) malloc(sizeof(t_no)); //pois malloc devolve void*, um ponteiro genérico
     if (no != NULL){
@@ -17,10 +22,27 @@ t_no * constroi_no (int info) {
     return no;
 }
 
+void inicializa_lista (t_lista * lista) {
+    lista->primeiro = NULL;
+    lista->ultimo = NULL;
+}
+
+// int main() {
+//     t_no * no1 = constroi_no(10);
+//     printf("valor de no1: %p\n", no1);
+//     printf("conteudo apontado por no1 no campo info: %d\n", no1->info);
+//     printf("onde esta a variavel no1: %p\n", &no1);
+//     printf("tamanho da variavel no1: %d\n", sizeof(no1));
+//     printf("conteudo apontado por no1: %d\n", sizeof(*no1));
+//     return 0;
+// }
+
 int main() {
-    t_no * no1 = constroi_no(10);
-    printf("valor de no1: %p\n", no1);
-    printf("conteudo apontado por no1 no campo info: %d\n", no1->info);
-    printf("onde esta a variavel no1: %p\n", &no1);
+    t_lista lista;
+    printf("primeiro antes: %p\n", lista.primeiro);
+    printf("ultimo antes: %p\n", lista.ultimo);
+    inicializa_lista(&lista);
+    printf("primeiro depois: %p\n", lista.primeiro);
+    printf("ultimo depois: %p\n", lista.ultimo);
     return 0;
 }
